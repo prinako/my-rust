5ca3fe05322ffbdcaef14f6c95bcfe560ff5b476
prinako <nyankonton122@gmail.com>
2024-01-16 11:22:14 -0300

    user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("Say Something");
+    match io::stdin().read_line(&mut input){
+        Ok(_) => {
+            println!("You said {}", input);
+        },
+        Err(e)=>{
+            println!("Something went wrong {}",e);
+        }
+    }
 }
